3f4a68d1a772f458dbccbb12097884fc771123ed
shabanashaik2509 <32096165+shabanashaik2509@users.noreply.github.com>
2023-07-06 12:33:54 +0530

    Create main.py
    Signed-off-by: shabanashaik2509 <32096165+shabanashaik2509@users.noreply.github.com>

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def hello_world(request):
+    """
+    Args:
+        request (flask.Request): The request object
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+    """
+    return "Hello World!"
